b859f10e8c609c3ab98b3381fbb986eda606a3f3
unknown <emqrrva@CN00202740.ericsson.se>
2017-04-20 09:54:32 +0800

    add getter and setter

--- src/demo1/Test3.java
+++ src/demo1/Test3.java
@@ -3,5 +3,14 @@ package demo1;
 public class Test3 {
 
 	private int index;
+
+	public int getIndex() {
+		return index;
+	}
+
+	public void setIndex(int index) {
+		this.index = index;
+	}
+	
 	
 }

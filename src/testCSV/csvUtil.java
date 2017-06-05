840f2a724476022445bdad0930f8c2008c77a39d
John <zheng.a.lei@ericsson.com>
2017-06-05 17:36:38 +0800

    test transfer csv file

---
+++ src/testCSV/csvUtil.java
@@ -0,0 +1,20 @@
+package testCSV;
+
+import java.io.File;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class csvUtil {
+
+	/*
+	 * @param dataList: data source
+	 * @param titleMap: data table tile
+	 * @param outputPath: csv file path
+	 * @param fileName: csv file name
+	 */
+	public static File createCSVFile(List dataList, LinkedHashMap titleMap,
+			String outputPath, String fileName) {
+
+		return null;
+	}
+}
